c262d759a060a2a758e269a335ab64389516b5d9
Kashyab19 <kashyabnarayanan3721@gmail.com>
2020-07-05 18:52:09 +0530

    Day 3 Lecture 6

--- app.js
+++ app.js
@@ -4,10 +4,20 @@ const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
 const mongoose = require('mongoose');
 const app = express();
+const bcrpyt = require('bcryptjs')
 
 const Event = require("./models/event")
+const User = require("./models/user")
 
-const events = [];
+const user = userId =>{
+  return user.findById(userId)
+  .then(user =>{
+    return {...user._doc,_id:user.id}
+  })
+  .catch(err =>{
+    throw err;
+  })
+}
 
 app.use(bodyParser.json());
 
@@ -21,6 +31,13 @@ app.use(
           description: String!
           price: Float!
           date: String!
+          creator :User!
+        }
+        type User {
+          _id: ID!
+          email: String!
+          password: String
+          createdEvents : [Event!]
         }
         input EventInput {
           title: String!
@@ -28,11 +45,16 @@ app.use(
           price: Float!
           date: String!
         }
+        input UserInput {
+          email: String!
+          password: String!
+        }
         type RootQuery {
             events: [Event!]!
         }
         type RootMutation {
             createEvent(eventInput: EventInput): Event
+            createUser(userInput: UserInput): User
         }
         schema {
             query: RootQuery
@@ -41,35 +63,73 @@ app.use(
     `),
     rootValue: {
       events: () => {
-        return Event.find()
-        .then(events => {return events.map(event =>{
-          return {...event._doc,_id:event._doc._id.toString()}
-        })
-      })
-        .catch(err =>{console.log("Error at Line 44"+err)})
+        return Event.find().populate('creator')
+          .then(events => {
+            return events.map(event => {
+              return { ...event._doc, 
+                _id: event.id,
+              creator:{
+                ...event._doc.creator._doc,
+                _id:event._doc.creator.id
+              } };
+
+            });
+          })
+          .catch(err => {
+            throw err;
+          });
       },
       createEvent: args => {
-        // const event = {
-        //   _id: Math.random().toString(),
-        //   title: args.eventInput.title,
-        //   description: args.eventInput.description,
-        //   price: +args.eventInput.price,
-        //   date: args.eventInput.date
-        // };
         const event = new Event({
           title: args.eventInput.title,
           description: args.eventInput.description,
           price: +args.eventInput.price,
-          date: new Date(args.eventInput.date)
-        })
-        return event.save()
-        .then(result => {console.log(result)
-        return{...result._doc,_id:event._doc._id.toString()}
-              })
-        .catch(err =>{console.log(err)
-        throw err;
-              })
-        
+          date: new Date(args.eventInput.date),
+          creator: '5f0174eca850d304f400cfce'
+        });
+        let createdEvent;
+        return event
+          .save()
+          .then(result => {
+            createdEvent = { ...result._doc, _id: result._doc._id.toString() };
+            return User.findById('5f0174eca850d304f400cfce');
+          })
+          .then(user => {
+            if (!user) {
+              throw new Error('User not found.');
+            }
+            user.createdEvents.push(event);
+            return user.save();
+          })
+          .then(result => {
+            return createdEvent;
+          })
+          .catch(err => {
+            console.log(err);
+            throw err;
+          });
+      },
+      createUser: args => {
+        return User.findOne({ email: args.userInput.email })
+          .then(user => {
+            if (user) {
+              throw new Error('User exists already.');
+            }
+            return bcrypt.hash(args.userInput.password, 12);
+          })
+          .then(hashedPassword => {
+            const user = new User({
+              email: args.userInput.email,
+              password: hashedPassword
+            });
+            return user.save();
+          })
+          .then(result => {
+            return { ...result._doc, password: null, _id: result.id };
+          })
+          .catch(err => {
+            throw err;
+          });
       }
     },
     graphiql: true
