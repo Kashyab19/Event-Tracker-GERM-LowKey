5f4561dae7c1737611e527b7fd492834543ad885
Kashyab19 <kashyabnarayanan3721@gmail.com>
2020-07-09 09:47:39 +0530

    Day Lecture 11
    Adding Basic FrontEnd and Routes

---
+++ frontend/src/pages/Booking.js
@@ -0,0 +1,7 @@
+import React,{Component} from 'react';
+
+export default class BookingPage extends Component{
+    render(){
+        return(<h1>Bookingorisation Page</h1>);
+    }
+}
---
+++ frontend/src/pages/Events.js
@@ -0,0 +1,7 @@
+import React,{Component} from 'react';
+
+export default class EventPage extends Component{
+    render(){
+        return(<h1>Eventorisation Page</h1>);
+    }
+}
